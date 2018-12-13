b0b3f236a1e20cda96a9fa6374cd5537dbf01d90
Sean Kinread <sean@kinread.com>
2018-12-13 16:16:05 +1100

    Now v2 config with dynamic routes, next.config work around for server build issue

--- next.config.js
+++ next.config.js
@@ -1,2 +1,16 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : process.env.NOW_REGION === false
+      ? require('next/constants')
+      : require('next-server/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withSass = require('@zeit/next-sass');
+  return withSass();
+};
